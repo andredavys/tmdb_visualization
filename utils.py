479479dfaa18665ffa48fc23327bf2ebfe6ca333
André Davys <adavys06@hotmail.com>
2020-05-23 16:41:24 -0300

    Add files via upload
    add initial visualizations and update utils.py

--- utils.py
+++ utils.py
@@ -1,3 +1,19 @@
+import pandas as pd
+
+def pre_processing_movies_data(df_movies):
+    df_movies.release_date = df_movies.release_date.map(lambda x: str(x))
+    df_movies.budget = pd.to_numeric(df_movies.budget, errors='coerce')
+    df_movies.revenue = pd.to_numeric(df_movies.revenue, errors='coerce')
+    df_movies.release_date = pd.to_datetime(df_movies.release_date, errors='coerce')
+    df_movies.popularity = pd.to_numeric(df_movies.popularity, errors='coerce')
+
+    # datarange by year
+    df_movies = df_movies[(df_movies.release_date.dt.year > 1965) & (df_movies.release_date.dt.year < 2017)]
+
+    # groupby movies by month
+    df_movies.set_index(['release_date'], inplace=True)
+    return df_movies
+
 def get_genres_from_row(row):
     aux_row = row[1:-1]
     raw_genres = aux_row.split('}')
@@ -35,7 +51,7 @@ def mount_movies_dict(df):
             genres=get_genres_from_row(row['genres']),
             runtime=float(row['runtime']),
             popularity=float(row['popularity']),
-            profit=float(row['budget'])-float(row['revenue'])
+            profit=float(row['revenue'])-float(row['budget'])
         )   
         movies_dict[id_movie] = inner_dict
     
@@ -53,3 +69,28 @@ def mount_genres_count_dict(movies_dict):
                 genres_count_dict[genre] = 1
     genres_count_dict = {k: v for k, v in sorted(genres_count_dict.items(), key=lambda item: item[1], reverse=True)}
     return genres_count_dict
+
+# to do visualization by overtime
+def create_day_month_year_column(df):
+    days = list()
+    months = list()
+    years = list()
+    for i, row in df.iterrows():
+        date = str(i)
+        years.append(date.split('-')[0])
+        months.append(date.split('-')[1])
+        days.append(date.split('-')[2])
+    new_df = df.copy()
+    print(len(days), len(months), len(years), len(new_df))
+    new_df['day'] = days
+    new_df['month'] = months
+    new_df['year'] = years
+    return new_df
+
+def normalize_count_by_year(df):
+    norm_df = df.copy()
+    norm_count = list()
+    for year in df.year.unique():
+        y = df[(df.year==str(year))].aux_count
+        df.loc[y.index, 'norm_count'] = y.values/sum(y)
+    return df
